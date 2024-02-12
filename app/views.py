55096e05398308d78d08269f4667e09d099490cf
BARATH-20002001 <bharathsarangan1@gmail.com>
2024-02-12 18:07:15 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from app.models import *
 
 class SchoolList(ListView):
@@ -10,3 +10,8 @@ class SchoolList(ListView):
     ordering=['sname']
     #queryset=School.objects.filter(id=1)
     #template_name='app/school_list.html'
+
+
+class SchoolView(DetailView):
+    model=School
+    context_object_name='sclobject'
